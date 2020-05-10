8a3562ed3462d51da71226d49826a4e3a782cb63
cloudgang <35246400+CloudGang@users.noreply.github.com>
2020-05-10 19:04:35 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+const buttons = document.querySelectorAll('.btn')
+buttons.forEach(button => {
+    button.addEventListener('mouseover', () => {
+        button.style.transform = 'scale(1.25)'
+        button.style.backgroundColor = 'black'
+    })
+})
+
+buttons.forEach(button => {
+    button.addEventListener('mouseout', () => {
+        button.style.transform = 'scale(1)'
+        button.style.backgroundColor = '#17A2B8'
+    })
+})
